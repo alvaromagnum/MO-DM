0cb6d651166518f21fe111961a5d6250bbacf2a6
Alvaro Magnum <alvaromagnum@gmail.com>
2023-10-13 00:30:59 -0300

    Adding decision pages - PART 5

--- public/js/my-decisions.js
+++ public/js/my-decisions.js
@@ -30,60 +30,86 @@ function importDefaultData() {
 
             for(var decision of data.decisions) {
 
-                var row = $("<tr></tr>").html(`
-                    <td>
-                        <div class="d-flex px-2 py-1">
-                          <div>
-                            <i class="material-icons opacity-10">not_listed_location</i>&nbsp;
-                          </div>
-                          <div class="d-flex flex-column justify-content-center">
-                            <h6 class="mb-0 text-sm">${decision.question}</h6>
-                          </div>
+                var card = $("<div></div>").html(`
+                    <div class="card">
+                      <div class="card-body px-0 pb-2">
+                        <div class="table-responsive">
+                          <table class="table align-items-center mb-0">
+                            <thead>
+                            <tr>
+                              <th class="text-uppercase text-secondary text-xx font-weight-bolder opacity-7">Decisão</th>
+                              <th class="text-uppercase text-secondary text-xx font-weight-bolder opacity-7 ps-2">Expectativa</th>
+                              <th class="text-uppercase text-secondary text-xx font-weight-bolder opacity-7 ps-2">Valor</th>
+                              <th class="text-uppercase text-secondary text-xx font-weight-bolder opacity-7 ps-2">Custo</th>
+                              <th class="text-uppercase text-secondary text-xx font-weight-bolder opacity-7 ps-2">Opções</th>
+                            </tr>
+                            </thead>
+                            <tbody id="tableQuestions_${decision.decisionId}">
+                                <tr>
+                                    <td>
+                                        <div class="d-flex px-2 py-1">
+                                          <div>
+                                            <i class="material-icons opacity-10">not_listed_location</i>&nbsp;
+                                          </div>
+                                          <div class="d-flex flex-column justify-content-center">
+                                            <h6 class="mb-0 text-sm">${decision.question}</h6>
+                                          </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="mt-2">
+                                          <select id="selectExpectancy_${decision.decisionId}" class="star-rating">
+                                            <option value="0">0</option>
+                                            <option value="6">6</option>
+                                            <option value="5">5</option>
+                                            <option value="4">4</option>
+                                            <option value="3">3</option>
+                                            <option value="2">2</option>
+                                            <option value="1">1</option>
+                                          </select>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="mt-2">
+                                          <select id="selectValue_${decision.decisionId}" class="star-rating">
+                                            <option value="0">0</option>
+                                            <option value="6">6</option>
+                                            <option value="5">5</option>
+                                            <option value="4">4</option>
+                                            <option value="3">3</option>
+                                            <option value="2">2</option>
+                                            <option value="1">1</option>
+                                          </select>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="mt-2">
+                                          <select id="selectCost_${decision.decisionId}" class="star-rating">
+                                            <option value="0">0</option>
+                                            <option value="6">6</option>
+                                            <option value="5">5</option>
+                                            <option value="4">4</option>
+                                            <option value="3">3</option>
+                                            <option value="2">2</option>
+                                            <option value="1">1</option>
+                                          </select>
+                                        </div>
+                                    </td>
+                                    <td class="text-center">
+                                        <button id="btZoomOut" class="btn btn-outline-white align-bottom margin-top-10" data-toggle="tooltip" title="Adicionar Opção"><i class="material-icons icon-button">add</i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                          </table>
                         </div>
-                    </td>
-                    <td>
-                        <div class="mt-2">
-                          <select id="selectExpectancy${decision.decisionId}" class="star-rating">
-                            <option value="0">0</option>
-                            <option value="6">6</option>
-                            <option value="5">5</option>
-                            <option value="4">4</option>
-                            <option value="3">3</option>
-                            <option value="2">2</option>
-                            <option value="1">1</option>
-                          </select>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="mt-2">
-                          <select id="selectValue${decision.decisionId}" class="star-rating">
-                            <option value="0">0</option>
-                            <option value="6">6</option>
-                            <option value="5">5</option>
-                            <option value="4">4</option>
-                            <option value="3">3</option>
-                            <option value="2">2</option>
-                            <option value="1">1</option>
-                          </select>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="mt-2">
-                          <select id="selectCost${decision.decisionId}" class="star-rating">
-                            <option value="0">0</option>
-                            <option value="6">6</option>
-                            <option value="5">5</option>
-                            <option value="4">4</option>
-                            <option value="3">3</option>
-                            <option value="2">2</option>
-                            <option value="1">1</option>
-                          </select>
-                        </div>
-                    </td>
-                    <td class="text-center">---</td>
+                      </div>
+                    </div>
+                    <br/><br/>
+                    <hr class="horizontal light mt-0 mb-2">
+                    <br/><br/>
                 `);
 
-                $("#tableQuestions").append(row);
+                $("#decisionCards").append(card);
 
             }
 
